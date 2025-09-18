a242121f0a8215f0e5ea4e9170784ab8662c90d0
Jennifer Olsen <jenniferolsen@sandiego.edu>
2025-09-17 20:11:01 -0700

    initial starter code

--- art.py
+++ art.py
@@ -18,7 +18,7 @@ def main():
     root = tk.Tk()
     root.title("USD COMP110 XXXX")
 
-    canvas = tk.Canvas(root, width=450, height=150, bg="white")
+    canvas = tk.Canvas(root, width=500, height=500, bg="white")
     canvas.pack()
 
     #place function call(s) here
